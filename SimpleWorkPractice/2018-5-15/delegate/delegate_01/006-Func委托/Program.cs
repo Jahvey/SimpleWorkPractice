87dbb45a9a95ff60b568b72fafedaed5a6edeae8
2807867265@qq.com <2807867265@qq.com>
2018-05-15 23:40:53 +0800

    2018-5-15 00 23:40 commit for delegate code.commit by adonai.

---
+++ SimpleWorkPractice/2018-5-15/delegate/delegate_01/006-Func委托/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _006_Func委托
+{
+    class Program
+    {
+
+        internal static int Test1() {
+            return 22;
+        }
+
+
+        private static int Test3(int i1,int i2) {
+
+            return i1 + i2;
+        }
+
+        static void Main(string[] args)
+        {
+
+            Func<int> a=Test1;//如果Func中的泛型类型指定的只有一个参数，那么就表明指向的 委托方法的返回值类型
+
+            Console.WriteLine(a());
+            //Func后面可以跟很多类型，最后一个类型是返回值类型（必须有，Action则必没有返回值）
+            //参数类型必须要跟指向的方法的参数类型按照顺序对应。
+            Func<String,int> b = Test2;
+            Console.WriteLine(b("haha"));
+            Func<int, int, int> c = Test3;
+            Console.WriteLine(c(2,4));
+
+            Console.ReadKey();
+
+        }
+
+        private static int Test2(string arg)
+        {
+            return arg.GetHashCode();
+        }
+    }
+}
---
+++ SimpleWorkPractice/2018-5-15/delegate/delegate_01/delegate_01/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegate_01
+{
+    class Program
+    {
+        //委托是指向方法的应用，指向的是方法
+        private delegate string     GetAString();
+        //
+        private delegate void PrintString();
+
+        static void Main(string[] args)
+        {
+
+           // test01();
+            //test02();
+            test03();
+
+
+
+        }
+
+        private static void Method1() {
+
+            Console.WriteLine("Method1");
+        }
+
+
+        private static void Method2()
+        {
+
+            Console.WriteLine("Method2");
+        }
+        private static void PrintStr(PrintString ps) {
+
+            ps.Invoke();
+        }
+
+        /// <summary>
+        /// 实例2 使用委托类型作为方法的参数
+        /// </summary>
+        private static void test03() {
+
+            PrintString ps = Method1;
+            PrintStr(ps);
+             ps = Method2;
+            PrintStr(ps);
+            Console.ReadKey();
+
+        
+        }
+
+
+        /// <summary>
+        /// 测试委托
+        /// </summary>
+        private static void test02()
+        {
+            int x = 40;
+            //a指向了x的tostring方法
+            GetAString a = x.ToString;//注意，这里是将x.ToString方法的引用传递给委托
+            Console.WriteLine(a.Invoke());//通过invoke方法来调用a所引用的方法
+            Console.WriteLine(a());
+            Console.ReadKey();
+        }
+
+
+        /// <summary>
+        /// 测试委托
+        /// </summary>
+        private static void test01()
+        {
+            int x = 40;
+            //a指向了x的tostring方法
+            GetAString a = new GetAString(x.ToString);//注意，这里是将x.ToString方法的引用传递给委托
+            Console.WriteLine(a.Invoke());
+            Console.WriteLine(a());
+            Console.ReadKey();
+        }
+    }
+}
